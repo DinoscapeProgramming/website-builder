604255aa7dc6100d8c88b8fba2f81185882975e0
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2022-04-10 20:14:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
 build(
-    [
-       [
-            "head",
-            {
-                title: "Welcome"
-            }
-       ],
-       [
-            "body",
-            {
-                childs: [
-                    [
-                        "h1",
-                        {
-                            innerHTML: "TTS"
-                        }
-                    ],
-                    [
-                        "p",
-                        {
-                            innerHTML: "TTS (text to speech) is an easy to use chrome extension/add-on. Simply select some text and read it loudly."
-                        }
-                    ]
-                ]
-            }
-        ]
-    ],
-    {
-      lang: "en"
-    }
+    [],
+    {}
 )
